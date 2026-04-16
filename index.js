4ed04113c2c3955e47cf358efbf0ffb117c7b095
mitsugu oyama <mitsugu.oyama@gmail.com>
2026-04-16 20:34:50 +0900

    第一部、第二部のビジュアライズ追加

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // ページ読み込み時にフワッと要素を表示させる上品なアニメーション
     const fadeElements = document.querySelectorAll('.fade-in');
     
-    // Intersection Observerを使って、画面に入ったら表示（今回は全体がほぼ1画面に収まる想定ですが汎用性を持たせています）
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 entry.target.classList.add('visible');
-                observer.unobserve(entry.target); // 一度表示されたら監視を解除
+                observer.unobserve(entry.target);
             }
         });
     }, {
